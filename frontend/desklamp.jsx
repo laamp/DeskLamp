d4959a292cd004251c856643f14a2d1b594a6710
Lance Smith <lance.matthew.smith@gmail.com>
2019-04-30 16:44:31 -0700

    Setup webpack and misc

---
+++ frontend/desklamp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.getElementById('root');
+  ReactDOM.render(<h1>DeskLamp: React Component</h1>, rootElement);
+});
