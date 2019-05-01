bc0ee797394589fbbf7f05efdb9c3240f66cf28a
Lance Smith <lance.matthew.smith@gmail.com>
2019-04-30 17:32:07 -0700

    Migrate users table and react setup

--- db/migrate/20190430231757_create_users.rb
+++ db/migrate/20190430231757_create_users.rb
@@ -1,8 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :email, null: false
+      t.string :name, null: false
+      t.string :company_name
+      t.string :job_title
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :avatar_url
       t.timestamps
     end
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
   end
 end
