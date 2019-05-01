57efda0422b1078f71abb808b4525b6ed822438e
Lance Smith <lance.matthew.smith@gmail.com>
2019-04-30 17:59:52 -0700

    End of day commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  attr_reader :password
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
