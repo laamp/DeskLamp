6c46f66a65894b586c447e1d33cd636db983bd4e
Lance Smith <lance.matthew.smith@gmail.com>
2019-04-30 23:02:07 -0700

    Create users controller and add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
   attr_reader :password
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
