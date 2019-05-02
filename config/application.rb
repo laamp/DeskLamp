1d942737b5ac8f760b6f3fedc4b77b1735d6e33e
Lance Smith <lance.matthew.smith@gmail.com>
2019-05-01 17:27:47 -0700

    Precompile fix

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 
 module DeskLamp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
