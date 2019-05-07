99c18a311e53e6cba513912f9a47c1f04f44bab8
Lance Smith <lance.matthew.smith@gmail.com>
2019-05-06 22:40:23 -0700

    CSS madness on the splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -7,7 +7,7 @@ export const SplashComponent = () => {
       <header>
         <div className="header-left">
           <Link to='/'>
-            <div className="company-logo"></div>
+            <img className="company-logo" src="/assets/basecamp-logo.png" />
             <h1 id="company-name">DeskLamp</h1>
           </Link>
         </div>
@@ -16,14 +16,25 @@ export const SplashComponent = () => {
           <Link id='sign-up-button' to='/signup'>Try It FREE</Link>
         </nav>
       </header>
-
-      <section className="about-section">
-        <img src="/assets/50-mil.jpg" />
-
+      <section className="splash-content">
+        <section className="about-section-container">
+          <section className="about-section">
+            <img src="/assets/50-mil.jpg" />
+            <p>
+              <strong>
+                That’s the number of projects that have been managed with Basecamp.&nbsp;
+              </strong>
+              Companies trust Basecamp because it’s *the* gold-standard for managing
+              projects, working with clients, and communicating with your team.
+              Don’t settle for imitators and also-rans.
+              If you want to finish projects faster with less chaos and confusion,
+              there’s only Basecamp.
+            </p>
+          </section>
+        </section>
+        <section className="testimonials-section"></section>
+        <section className="footer-section"></section>
       </section>
-      <section className="testimonials-section"></section>
-      <section className="footer-section"></section>
-      <h1>DeskLamp Splash Page</h1>
     </>
   );
 };
