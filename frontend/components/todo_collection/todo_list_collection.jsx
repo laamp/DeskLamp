c48a223cf8ab5728c25f7aa11f70d5bba8451c7f
Lance Smith <lance.matthew.smith@gmail.com>
2019-06-21 19:29:33 -0700

    Setup rendering of todo lists and their tasks

--- frontend/components/todo_collection/todo_list_collection.jsx
+++ frontend/components/todo_collection/todo_list_collection.jsx
@@ -19,14 +19,16 @@ class TodoListCollection extends React.Component {
   componentDidMount() {
     this.setState({ hubId: this.props.currentCollection.hubId });
     this.props.fetchAllLists(this.props.collectionId).then(({ todo_lists }) => {
-      let listIds = Object.keys(todo_lists);
+      let listIds = Object.keys(todo_lists).map(num => num = parseInt(num));
       let taskIds = [];
       for (let i = 0; i < listIds.length; i++) {
-        this.props.fetchAllTasks(this.props.collectionId, listIds[i]).then(asdf => {
-          // console.log(asdf);
+        this.props.fetchAllTasks(this.props.collectionId, listIds[i]).then(({ todo_tasks }) => {
+          todo_tasks.forEach(task => taskIds.push(task.id));
         });
       }
-      this.setState({ loading: false, listIds });
+      this.setState({ loading: false, listIds, taskIds });
+      window.tasks = this.props.todoTasks;
+      window.taskIds = this.state.taskIds;
       manualSave();
     });
   }
@@ -46,11 +48,34 @@ class TodoListCollection extends React.Component {
 
           <section id="todo-collection-body">
             <section id="todo-collection-body-title">
-              <Link id="create-button" to="/todo_lists/new">
+              {/* <Link id="create-button" to="/organizations">
                 <p>Create new todo list</p>
-              </Link>
+              </Link> */}
               <h1>Todo Lists</h1>
             </section>
+
+            <ul className="todo-lists">
+              {this.state.listIds.map(i =>
+                <li key={i}>
+                  <p>
+                    <span>
+                      {this.props.todoLists[i].name}
+                    </span>
+                    &nbsp;—&nbsp;
+                    {this.props.todoLists[i].details}
+                  </p>
+
+                  {this.props.todoTasks.filter(globalTask => {
+                    if (globalTask.todo_list_id === i) return true;
+                  }).map(task =>
+                    <section key={task.id}>
+                      <p>{task.name}</p>
+                    </section>
+                  )}
+                </li>
+              )}
+            </ul>
+
           </section>
         </section>
       </>
